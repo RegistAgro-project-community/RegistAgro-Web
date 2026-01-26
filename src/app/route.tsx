b204ff810f3d749f3d3ca8dadc8ac6473060bd85
Leginio <leginiosebastiao@gmail.com>
2026-01-26 01:18:50 +0100

    Organização da estrutura das pastas

---
+++ src/app/route.tsx
@@ -0,0 +1,20 @@
+import { BrowserRouter ,Route, Routes } from "react-router-dom";
+import Login from "./auth/login/app";
+import Register from "./auth/register/app";
+import Home from "./dashboard/app";
+import Produtos from "./produtos/app";
+export function AppRoutes() {
+  return (
+    <BrowserRouter>
+    
+    <Routes>
+         <Route path="/login" element={<Login/>}></Route>
+         <Route path="/register" element={<Register/>}></Route>
+         <Route path="/dashboard" element={<Home/>}></Route>  
+         <Route path="/produtos" element={<Produtos/>}></Route>
+
+    </Routes>
+    
+    </BrowserRouter>
+  );
+}
