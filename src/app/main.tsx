137222fb58183e7a40dd569050f1aeb071da76fb
Leginio <leginiosebastiao@gmail.com>
2026-01-27 21:22:20 +0100

    Instalação do @fontsource/manrope

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -3,6 +3,10 @@ import { createRoot } from "react-dom/client";
 import "../app/style/global.css";
 import { AppRoutes } from "./route";
 import "material-symbols/outlined.css";
+import '@fontsource/manrope/400.css';
+import '@fontsource/manrope/500.css';
+import '@fontsource/manrope/700.css';
+import '@fontsource/manrope/800.css';
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <AppRoutes />
