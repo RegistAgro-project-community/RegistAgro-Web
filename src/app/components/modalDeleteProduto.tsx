2f4a2d14cef9ac99b985153e5023608761848e55
Leginio <leginiosebastiao@gmail.com>
2026-01-31 02:10:11 +0100

    Inicio da construção dos model de edit e delete de produto

---
+++ src/app/components/modalDeleteProduto.tsx
@@ -0,0 +1,8 @@
+type AddPrudutoProps = {
+  open: boolean;
+  children?: React.ReactNode;
+};
+function DeleteProduto({ open, children }: AddPrudutoProps) {
+  return <div></div>;
+}
+export default DeleteProduto;
---
+++ src/app/components/modalEditProduto.tsx
@@ -0,0 +1,9 @@
+type AddPrudutoProps = {
+  open: boolean;
+  children?: React.ReactNode;
+};
+
+function EditProduto({ open, children }: AddPrudutoProps) {
+  return <div></div>;
+}
+export default EditProduto;
