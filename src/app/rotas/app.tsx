b405c65ae0a9ae8fa484da2fbb9d5535fc357963
Leginio <leginiosebastiao@gmail.com>
2026-03-14 15:33:50 +0100

    Correção do Json-server e Pagina do Rotas dinâmica

--- src/app/rotas/app.tsx
+++ src/app/rotas/app.tsx
@@ -1,9 +1,60 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Nav from "../../components/sideBar/sideBar";
 import DetalhePedido from "../../components/modalDetalhePedido";
+import Cookies from "js-cookie";
+import { useTracking } from "../../hooks/useTracking/useCarrier";
+interface OrderData {
+  id: string;
+  client: string;
+  carrier: string;
+  date: string;
+  status: string;
+}
 export default function Rotas() {
   const [siderAberto, setSiderAberto] = useState(false);
   const [abertoDetalhe, setAbertoDetalhe] = useState(false);
+  const [waitingPickup, setIsWaitingPickup] = useState("");
+  const [inTransit, setIsInTrasit] = useState("");
+  const [delivered, setIsDelivered] = useState("");
+  const [orders, setIsOrders] = useState<OrderData[]>([]);
+  const token = Cookies.get("token");
+  const [correntPage, setIsCorrentPage] = useState(1);
+  const itemsPerPage = 5;
+  const [searchOrder, setIsSearchOrder] = useState("");
+  const { data } = useTracking(token);
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  function handleSearch(e: any) {
+    setIsSearchOrder(e.target.value);
+    setIsCorrentPage(1);
+    console.log(searchOrder);
+  }
+  useEffect(() => {
+    if (data && token) {
+      // eslint-disable-next-line react-hooks/set-state-in-effect
+      setIsInTrasit(data.cardsData.inTransit.toString() || "0");
+      setIsWaitingPickup(data.cardsData.waitingPickup.toString() || "0");
+      setIsDelivered(data.cardsData.delivered.toString() || "0");
+      setIsOrders([...data.orders]);
+    }
+  }, [token, data]);
+  console.log(orders);
+
+  const filteredOrders = orders
+    .filter((item) => {
+      const order = searchOrder.toLowerCase();
+      return (
+        item.client.toLowerCase().includes(order) ||
+        item.carrier.toLowerCase().includes(order) ||
+        item.status.toLowerCase().includes(order)
+      );
+    })
+    .reverse();
+
+  const totalItems = filteredOrders.length;
+  const totalPages = Math.ceil(totalItems / itemsPerPage);
+  const indexOfLastPage = correntPage * itemsPerPage;
+  const indexOfFristPage = indexOfLastPage - itemsPerPage;
+  const correntItems = filteredOrders.slice(indexOfFristPage, indexOfLastPage);
   return (
     <>
       <div className="bg-background text-text-main">
@@ -35,17 +86,17 @@ export default function Rotas() {
                 {[
                   {
                     label: "Aguardando Coleta",
-                    total: 3,
+                    total: waitingPickup || 0,
                     icon: "package_2",
                   },
                   {
                     label: "Em Trânsito",
-                    total: 8,
+                    total: inTransit || 0,
                     icon: "local_shipping",
                   },
                   {
                     label: "Entregues Hoje",
-                    total: 14,
+                    total: delivered || 0,
                     icon: "done_all",
                   },
                 ].map((item, i) => (
@@ -82,6 +133,8 @@ export default function Rotas() {
                       type="text"
                       className="w-full pl-10 pr-4 py-2 bg-background border-none rounded-lg text-sm focus:ring-2 focus:ring-primary/50 text-text-main placeholder-gray-400"
                       placeholder="Buscar cliente..."
+                      value={searchOrder}
+                      onChange={handleSearch}
                     />
                   </div>
                 </div>
@@ -89,9 +142,6 @@ export default function Rotas() {
                 <table className="w-full text-left border-collapse">
                   <thead>
                     <tr className="bg-background border-b border-border-color uppercase">
-                      <th className="px-6 py-4 text-xs font-semibold uppercase tracking-wider text-text-secondary">
-                        Pedido
-                      </th>
                       <th className="px-6 py-4 text-xs font-semibold uppercase tracking-wider text-text-secondary">
                         Destinátário
                       </th>
@@ -111,127 +161,118 @@ export default function Rotas() {
                     </tr>
                   </thead>
                   <tbody className="divide-y divide-border-color">
-                    {[
-                      {
-                        destinatario: "Restaurante Sabor",
-                        transportadora: "TransAgro Rápido",
-                        previsao: "14/10/2023",
-                        status: "aguardando coleta",
-                      },
-                      {
-                        destinatario: "Felipe João",
-                        transportadora: "Logística Verde",
-                        previsao: "14/10/2023",
-                        status: "em trânsito",
-                      },
-                      {
-                        destinatario: "Felipe João",
-                        transportadora: "TransAgro Rápido",
-                        previsao: "4/10/2023",
-                        preco: "10.000",
-                        status: "entregue",
-                      },
-                      {
-                        destinatario: "Felipe João",
-                        transportadora: "Logística Verde",
-                        previsao: "14/10/2023",
-                        preco: "10.000",
-                        status: "em trânsito",
-                      },
-                      {
-                        destinatario: "Felipe João",
-                        transportadora: "Expresso Rural",
-                        previsao: "14/10/2023",
-                        preco: "10.000",
-                        status: "aguardando coleta",
-                      },
-                    ].map((item, i) => (
-                      <tr
-                        key={i}
-                        className="hover:bg-gray-50 transition-colors group"
-                      >
-                        <td className="px-6 py-5">
-                          <p className="text-sm font-medium text-text-secondary">
-                            #{i + 1}
-                          </p>
-                        </td>
-                        <td className="px-6 py-5">
-                          <span className="text-sm font-semibold text-text-main ">
-                            {item.destinatario}
-                          </span>
-                        </td>
-                        <td className="px-6 py-5">
-                          <p className=" text-base font-medium text-text-secondary leading-relaxed capitalize">
-                            {item.transportadora}
-                          </p>
-                        </td>
-                        <td className="px-6 py-5">
-                          <p className="text-sm text-text-secondary font-medium ">
-                            {item.previsao}
-                          </p>
-                        </td>
+                    {correntItems && correntItems.length > 0 ? (
+                      correntItems.map((item, i) => (
+                        <tr
+                          key={i}
+                          className="hover:bg-gray-50 transition-colors group"
+                        >
+                          <td className="px-6 py-5">
+                            <span className="text-sm font-semibold text-text-main ">
+                              {item.client}
+                            </span>
+                          </td>
+                          <td className="px-6 py-5">
+                            <p className=" text-base font-medium text-text-secondary leading-relaxed capitalize">
+                              {item.carrier}
+                            </p>
+                          </td>
+                          <td className="px-6 py-5">
+                            <p className="text-sm text-text-secondary font-medium ">
+                              {item.date}
+                            </p>
+                          </td>
 
-                        <td className="px-6 py-5 text-center">
-                          <span
-                            className={`inline-flex items-center px-2.5 py-0 rounded-full text-xs font-medium ${item.status === "entregue" ? "bg-gray-100 text-gray-600  border-gray-200" : item.status === "em trânsito" ? "bg-blue-50 text-blue-600 border-blue-100" : item.status === "aguardando coleta" ? "bg-green-100 text-green-800 border-green-200" : ""}  border  capitalize`}
-                          >
-                            {item.status}
-                          </span>
-                        </td>
-                        <td className="px-6 py-4 ">
-                          <div
-                            className={`flex items-center  justify-end  gap-2`}
-                          >
-                            {item.status === "entregue" ? null : (
-                              <>
-                                <button className="inline-flex items-center gap-1.5 px-3 py-1.5 bg-white  border active:scale-93 border-border-color hover:border-primary hover:text-primary text-text-secondary text-xs font-medium rounded-lg transition-all">
+                          <td className="px-6 py-5 text-center">
+                            <span
+                              className={`inline-flex items-center px-2.5 py-0 rounded-full text-xs font-medium ${item.status === "delivered" ? "bg-gray-100 text-gray-600  border-gray-200" : item.status === "inTransit" ? "bg-blue-50 text-blue-600 border-blue-100" : item.status === "waitingPickup" ? "bg-green-100 text-green-800 border-green-200" : ""}  border  capitalize`}
+                            >
+                              {item.status}
+                            </span>
+                          </td>
+                          <td className="px-6 py-4 ">
+                            <div
+                              className={`flex items-center  justify-end  gap-2`}
+                            >
+                              {item.status === "delivered" ? null : (
+                                <>
+                                  <button className="inline-flex items-center gap-1.5 px-3 py-1.5 bg-white  border active:scale-93 border-border-color hover:border-primary hover:text-primary text-text-secondary text-xs font-medium rounded-lg transition-all">
+                                    <span className="material-symbols-outlined text-[16px]">
+                                      call
+                                    </span>
+                                    Entrar em contacto com o motorista
+                                  </button>
+                                  <button className="inline-flex items-center gap-1.5 px-3 py-1.5 bg-white  border  active:scale-93 border-border-color hover:border-primary hover:text-primary text-text-secondary text-xs font-medium rounded-lg transition-all">
+                                    <span className="material-symbols-outlined text-[16px]">
+                                      map
+                                    </span>
+                                    Ver no mapa
+                                  </button>
+                                </>
+                              )}
+                              {item.status !== "delivered" ? null : (
+                                <button
+                                  onClick={() => setAbertoDetalhe(true)}
+                                  className={`inline-flex items-center gap-1.5 px-3 py-1.5 bg-white dark:bg-surface-dark border active:scale-93  border-border-color hover:bg-gray-50 text-text-secondary text-xs font-medium rounded-lg transition-all`}
+                                >
                                   <span className="material-symbols-outlined text-[16px]">
-                                    call
+                                    visibility
                                   </span>
-                                  Entrar em contacto com o motorista
+                                  Detalhes
                                 </button>
-                                <button className="inline-flex items-center gap-1.5 px-3 py-1.5 bg-white  border  active:scale-93 border-border-color hover:border-primary hover:text-primary text-text-secondary text-xs font-medium rounded-lg transition-all">
-                                  <span className="material-symbols-outlined text-[16px]">
-                                    map
-                                  </span>
-                                  Ver no mapa
-                                </button>
-                              </>
-                            )}
-                            {item.status !== "entregue" ? null : (
-                              <button
-                                onClick={() => setAbertoDetalhe(true)}
-                                className={`inline-flex items-center gap-1.5 px-3 py-1.5 bg-white dark:bg-surface-dark border active:scale-93  border-border-color hover:bg-gray-50 text-text-secondary text-xs font-medium rounded-lg transition-all`}
-                              >
-                                <span className="material-symbols-outlined text-[16px]">
-                                  visibility
-                                </span>
-                                Detalhes
-                              </button>
-                            )}
+                              )}
+                            </div>
+                          </td>
+                        </tr>
+                      ))
+                    ) : (
+                      <tr>
+                        <td colSpan={8} className="py-20 text-center">
+                          <div className="flex flex-col items-center justify-center w-full">
+                            <p className="text-gray-500 font-medium">
+                              {orders.length === 0
+                                ? "Sem entregas no momento."
+                                : filteredOrders.length === 0
+                                  ? "Cliente não encontrado."
+                                  : ""}
+                            </p>
                           </div>
                         </td>
                       </tr>
-                    ))}
+                    )}
                   </tbody>
                 </table>
                 {/* Paginação */}
-                <div className="px-6 py-4 border-t border-border-color flex items-center justify-between w-full">
+                <div className="px-6 py-4 border-t border-border-color flex items-center justify-between">
                   <p className="text-medium text-gray-600">
                     Mostrando{" "}
-                    <span className="font-medium text-text-main">1</span> a{" "}
-                    <span className="font-medium text-text-main">5</span> de{" "}
-                    <span className="font-medium text-text-main">5</span>{" "}
-                    transportadoras
+                    <span className="font-medium text-text-main">
+                      {totalItems === 0 ? 0 : indexOfFristPage + 1}
+                    </span>{" "}
+                    a{" "}
+                    <span className="font-medium text-text-main">
+                      {Math.min(indexOfLastPage, totalItems)}
+                    </span>{" "}
+                    de{" "}
+                    <span className="font-medium text-text-main">
+                      {totalItems}
+                    </span>{" "}
+                    Produtos
                   </p>
                   <div className="flex gap-2">
                     <button
+                      onClick={() => setIsCorrentPage((prev) => prev - 1)}
                       className="px-3 py-1 text-sm font-medium text-gray-500 bg-white border border-gray-200 rounded-lg hover:bg-gray-100 disabled:opacity-50 active:scale-90"
-                      disabled
+                      disabled={correntPage === 1}
                     >
                       Anterior
                     </button>
-                    <button className="px-3 py-1 text-sm font-medium text-gray-500 bg-white border border-gray-200 rounded-lg hover:bg-gray-100 active:scale-90">
+
+                    <button
+                      onClick={() => setIsCorrentPage((prev) => prev + 1)}
+                      disabled={correntPage === totalPages || totalPages === 0}
+                      className="px-3 py-1 text-sm font-medium text-gray-500 bg-white border border-gray-200 rounded-lg hover:bg-gray-100 active:scale-90 disabled:opacity-50"
+                    >
                       Próximo
                     </button>
                   </div>
