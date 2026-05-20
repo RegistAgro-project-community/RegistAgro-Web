a7b46dc3fdcc5f7641c2875c335a25b2230eb210
Leginio <leginiosebastiao@gmail.com>
2026-05-20 12:38:14 +0100

    Hook para pegar as coordenadas do Carrier

---
+++ src/hooks/useGetLocationCarrier/useGetLocationCarriers.ts
@@ -0,0 +1,26 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "../../api/axios";
+
+export function useLocationCarrier(token: string | undefined, id: string) {
+  async function fetchLocationCarrier() {
+    try {
+      const { data } = await axios.get(
+        `/location/get/coordinates/order/${id}`,
+        {
+          headers: { Authorization: `Bearer ${token}` },
+        },
+      );
+      return data;
+    } catch (err) {
+      const error = err;
+      console.log(error);
+      return null;
+    }
+  }
+  return useQuery({
+    queryKey: ["Carrier", token],
+    queryFn: fetchLocationCarrier,
+    enabled: !!token,
+    refetchInterval: 30000,
+  });
+}
